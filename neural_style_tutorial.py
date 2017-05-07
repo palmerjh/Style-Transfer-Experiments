88916868fdec5f1870a937b371fbb2a01fcb3da3
Josh Palmer <joshua.h.palmer@vanderbilt.edu>
2017-05-06 21:48:07 -0500

    sdgsgsgs

--- neural_style_tutorial.py
+++ neural_style_tutorial.py
@@ -184,7 +184,7 @@ dtype = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
 
 
 # desired size of the output image
-imsize = 512 if use_cuda else 128  # use small size if no gpu
+imsize = 256 if use_cuda else 128  # use small size if no gpu
 
 loader = transforms.Compose([
     transforms.Scale(imsize),  # scale imported image
@@ -199,9 +199,9 @@ def image_loader(image_name):
     return image
 
 
-style_img = image_loader("styles/tree_square.jpg").type(dtype)
+style_img = image_loader("styles/stars_square.jpg").type(dtype)
 
-content_img = image_loader("suze_test/julianna_dog.jpg").type(dtype)
+content_img = image_loader("suze_test/dennis_dolphin.jpg").type(dtype)
 
 assert style_img.size() == content_img.size(), \
     "we need to import style and content images of the same size"
@@ -561,7 +561,7 @@ def get_input_param_optimizer(input_img):
 # the 0-1 interval.
 #
 
-def run_style_transfer(cnn, content_img, style_img, input_img, num_steps=300,
+def run_style_transfer(cnn, content_img, style_img, input_img, num_steps=600,
                        style_weight=1000, content_weight=1):
     """Run the style transfer."""
     print('Building the style transfer model..')
